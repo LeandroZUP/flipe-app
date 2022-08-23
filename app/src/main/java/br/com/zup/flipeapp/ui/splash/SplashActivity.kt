cfc603410ecf4262dbe97104fbb4250752b43fe3
Leandro Miguel <leandro.almeida@zup.com.br>
2022-08-23 13:53:57 -0300

    implement splashscreen code

--- app/src/main/java/br/com/zup/flipeapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/br/com/zup/flipeapp/ui/splash/SplashActivity.kt
@@ -1,12 +1,28 @@
 package br.com.zup.flipeapp.ui.splash
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.zup.flipeapp.R
+import java.util.*
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+    private val timer = Timer()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        timer.schedule(object: TimerTask() {
+            override fun run() {
+                jump()
+            }
+        }, 3000)
+    }
+
+    private fun jump() {
+        timer.cancel()
+        //TODO define next activity to exhibit
+        this.finish()
     }
 }
