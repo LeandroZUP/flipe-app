56374786957aa328a6af7603276c2b8283d4e79c
Leandro Miguel <leandro.almeida@zup.com.br>
2022-08-24 17:58:26 -0300

    develop and arranje login activity

--- app/src/main/java/br/com/zup/flipeapp/ui/splash/view/SplashActivity.kt
+++ app/src/main/java/br/com/zup/flipeapp/ui/splash/view/SplashActivity.kt
@@ -1,13 +1,20 @@
 package br.com.zup.flipeapp.ui.splash.view
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import br.com.zup.flipeapp.R
+import br.com.zup.flipeapp.ui.home.view.HomeActivity
+import br.com.zup.flipeapp.ui.login.view.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import java.util.*
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+    private val user: FirebaseAuth = Firebase.auth
     private val timer = Timer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +29,11 @@ class SplashActivity : AppCompatActivity() {
 
     private fun jump() {
         timer.cancel()
-        //TODO define next activity to exhibit
+        if (user.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        } else {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
         this.finish()
     }
 }
---
+++ app/src/main/java/br/com/zup/flipeapp/utilities/Constants.kt
@@ -0,0 +1,14 @@
+package br.com.zup.flipeapp.utilities
+
+const val USER_KEY = "user"
+const val NAME_ERROR_MESSAGE = "Favor informar nome"
+const val EMAIL_ERROR_MESSAGE = "Favor informar email"
+const val PASSWORD_ERROR_MESSAGE = "Favor informar senha"
+const val CREATE_USER_ERROR_MESSAGE = "Erro ao criar usuário!"
+const val LOGIN_ERROR_MESSAGE = "Não foi possivel realizar o login!"
+const val NAME_INCOMPLETE = "Favor o nome está incompleto"
+const val PASSWORD_INCOMPLETE = "A senha informada deve ter no mínimo 6 caracteres"
+const val EMAIL_INVALID = "E-mail inválido!"
+const val NAME_ALPHABETICAL_CHARACTERS = "Digite apenas caracteres alfabéticos no campo de nome"
+const val EMAIL_FORMAT = "@zup.com.br"
+const val HELLO = "Olá, "
