9cb27a97c75c7fae8583b73246de3be6ff411670
Leandro Miguel <leandro.almeida@zup.com.br>
2022-08-25 16:23:35 -0300

    Create icons

---
+++ app/src/main/java/br/com/zup/flipeapp/ui/home/viewmodel/HomeViewModel.kt
@@ -0,0 +1,15 @@
+package br.com.zup.flipeapp.ui.home.viewmodel
+
+import androidx.lifecycle.ViewModel
+import br.com.zup.flipeapp.domain.repository.AuthenticationRepository
+
+class HomeViewModel : ViewModel() {
+    private val authenticationRepository = AuthenticationRepository()
+
+    fun getDisplayName() = authenticationRepository.getDisplayName()
+
+    fun getUserEmail() = authenticationRepository.getEmailUser()
+
+    fun logoutUser() = authenticationRepository.logoutUser()
+
+}
