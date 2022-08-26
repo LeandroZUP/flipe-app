c6db99afd6617cf1a0b65ef6a20cc57c87a38766
Leandro Miguel <leandro.almeida@zup.com.br>
2022-08-26 20:46:16 -0300

    implement bookmark layout

---
+++ app/src/main/java/br/com/zup/flipeapp/ui/bookmark/view/BookmarkActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.flipeapp.ui.bookmark.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.flipeapp.R
+
+class BookmarkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmark)
+    }
+}
